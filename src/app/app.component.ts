7cd1f80892384d1dee6afcfabdd1bf1e896a4c99
Regina Eto Sobral Teixeira <regina@cds-software.com.br>
2019-10-14 15:31:46 -0300

    stylization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,12 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public mode = 'list';
   public todos: Todo[] = [];
   public title: String = "Tarefas";
   public form: FormGroup;
 
-  
+
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       title: ['', Validators.compose([
@@ -22,48 +23,56 @@ export class AppComponent {
       ])]
     });
     this.load();
-/*     this.todos.push(new Todo(1,'Passear com o doguinho', false));
-    this.todos.push(new Todo(2,'Ir ao mercado', true));
-    this.todos.push(new Todo(3,'Cortar cabelo', false)); */
+    /*     this.todos.push(new Todo(1,'Passear com o doguinho', false));
+        this.todos.push(new Todo(2,'Ir ao mercado', true));
+        this.todos.push(new Todo(3,'Cortar cabelo', false)); */
   }
 
-  add(){
+  add() {
     const title = this.form.controls['title'].value;
     const id = this.todos.length + 1;
-    this.todos.push(new Todo(id,title,false));
+    this.todos.push(new Todo(id, title, false));
     this.save();
     this.clear();
   }
 
-  clear(){
+  clear() {
     this.form.reset();
   }
 
-  remove(todo: Todo){
+  remove(todo: Todo) {
     const index = this.todos.indexOf(todo);
-    if(index !== -1){
+    if (index !== -1) {
       this.todos.splice(index, 1);
     }
     this.save();
   }
 
-  save(){
+  save() {
     const data = JSON.stringify(this.todos);
     localStorage.setItem('todos', data);
+    this.mode = 'list';
   }
 
-  load(){
+  load() {
     const data = localStorage.getItem('todos');
-    this.todos = JSON.parse(data);
+    if (data)
+      this.todos = JSON.parse(data);
+    else
+      this.todos = [];
   }
 
-  markAsDone(todo: Todo){
+  markAsDone(todo: Todo) {
     todo.done = true;
     this.save();
   }
 
-  markAsUndone(todo: Todo){
+  markAsUndone(todo: Todo) {
     todo.done = false;
     this.save();
+  }
+
+  changeMode(mode: string){
+    this.mode = mode;
   }
 }
