a51b66af663e7b0a73616880781c8a5beec85686
Regina Eto Sobral Teixeira <regina@cds-software.com.br>
2019-10-14 08:56:08 -0300

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title: String = "Tarefas";
+  public form: FormGroup;
+
+  
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+    this.todos.push(new Todo(1,'Passear com o doguinho', false));
+    this.todos.push(new Todo(2,'Ir ao mercado', true));
+    this.todos.push(new Todo(3,'Cortar cabelo', false));
+  }
+
+  remove(todo: Todo){
+    const index = this.todos.indexOf(todo);
+    if(index !== -1){
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo){
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo){
+    todo.done = false;
+  }
 }
